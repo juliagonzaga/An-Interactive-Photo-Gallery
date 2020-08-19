35f1867635795dd4f9b5ac6b073930e4ef3c8ec9
Julia <shaow.1230@gmail.com>
2020-08-19 20:59:40 +0300

    added javascript for search field

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
+const searchField = document.querySelector('input');
+const thumbnail = document.querySelectorAll('.thumbnail');
 
+searchField.addEventListener('keyup', (e) =>{
+    const userInput = e.target.value.toLowerCase();
 
+    for (let i = 0; i < thumbnail.length; i++){
+        let caption = thumbnail[i].getAttribute('data-caption').toLowerCase();
 
-
+        if (caption.includes(userInput)) {
+            thumbnail[i].style.display = '';
+        } else {
+            thumbnail[i].style.display = 'none';
+        }
+    }
+});
 
 window.addEventListener('load', function() {
     baguetteBox.run('.gallery', {
-        
+        animation: 'fadeIn'
     });
-  });
+});
